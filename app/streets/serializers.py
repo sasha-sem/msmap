9be8f77a4f9bebd118c876aaad30932f5898a547
Semenenko Sasha <alex.semenenko@live.ru>
2021-08-12 20:43:30 +0300

    added serializers for surface and street types

--- app/streets/serializers.py
+++ app/streets/serializers.py
@@ -3,11 +3,16 @@ from .models import Street, StreetType, SurfaceType
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
 
 
-class StreetSurfaceSerializer(serializers.ModelSerializer):
+
+class SurfaceTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = SurfaceType
         fields = '__all__'
 
+class StreetTypeSrializer(serializers.ModelSerializer):
+    class Meta:
+        model = StreetType
+        fields = '__all__'
 
 class StreetsSerializer(GeoFeatureModelSerializer):
     street_surface_rating = serializers.SlugRelatedField(
@@ -16,12 +21,13 @@ class StreetsSerializer(GeoFeatureModelSerializer):
         read_only=True,
         slug_field='rating'
     )
-
+    surface_type = SurfaceTypeSerializer(source='street_surface', many=False)
+    street_type = StreetTypeSrializer(many=False)
     class Meta:
         model = Street
         geo_field = "geom"
         fields = ('id',
-                  'street_surface',
                   'street_type',
+                  'surface_type',
                   'street_surface_rating'
                   )
